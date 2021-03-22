e2c825616e3dcc82f57b10ba0a3a4d8dbe790e8c
fxwing <http://fxwing@126.com>
2021-03-22 18:00:46 +0800

    '初始化'

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button} from 'antd'
+import LazyImage  from '@pages/LazyImage'
 
-function App() {
+
+const App: React.FC = ()=>{
+  const list = new Array(20).fill(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Button type="primary">测试antd</Button>
+      {list.map((i, index) =>
+      <LazyImage key={index}
+      style={{height:'100px'}}
+      src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa0.att.hudong.com%2F52%2F62%2F31300542679117141195629117826.jpg&refer=http%3A%2F%2Fa0.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1618992720&t=470d8c6c10c3e5407aedb23a902b4b80"
+      placeholder="这是一个图片地址" />)}
+    </main>
   );
 }
 
---
+++ src/pages/LazyImage/index.tsx
@@ -0,0 +1,130 @@
+import React, { useCallback, useState, useEffect, useRef } from 'react'
+import type { CSSProperties } from 'react'
+// import { Button } from 'antd'
+// import style from './index.less'
+
+export interface Iprops {
+  src?: string;
+  placeholder?: string;
+  className?: string;
+  style?: CSSProperties;
+  offset?: number;
+}
+
+export type TimeOut = ReturnType<typeof setTimeout>
+
+const LazyImage: React.FC<Iprops> = (props: Iprops) => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  const imgRef = useRef<HTMLDivElement>(null)
+  const handleScroll = throttle(_handleScroll, 500)
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleScroll);
+    handleScroll();
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleScroll);
+    }
+  }, [])
+  // 函数节流
+  function throttle(fn: () => void, delay: number): () => void {
+    let timer: TimeOut | null = null;
+    // let count: number = 0;
+    return function (this: any, ...argus) {
+      const context = this;
+      // 防抖
+      // if (timer){
+      //   clearTimeout(timer)
+      // }
+      // 节流
+      if (timer) return;
+      timer = setTimeout(function () {
+        // count++;
+        // console.log('这里执行了吗？', count)
+        fn.apply(context, argus);
+        clearTimeout(timer as TimeOut);
+        timer = null;
+      }, delay);
+    }
+  }
+
+  //获取窗口的高度
+  const getClientHeight = (): number => {
+    let clientHeight = 0;
+    if (document.documentElement.clientHeight && document.body.clientHeight) {
+      clientHeight = Math.min(document.documentElement.clientHeight, document.body.clientHeight)
+    } else {
+      clientHeight = Math.max(document.documentElement.clientHeight, document.body.clientHeight)
+    }
+    return clientHeight;
+  }
+  // 获取滚动的高度
+  const getScrollTop = (): number => {
+    let scrollTop: number = 0;
+    if (document.documentElement.scrollTop) {
+      scrollTop = document.documentElement.scrollTop
+    } else if (document.body.scrollTop) {
+      scrollTop = document.body.scrollTop
+    } else {
+      scrollTop = window.scrollY || window.pageYOffset
+    }
+    return scrollTop;
+  }
+
+  // 获取当前图片具体屏幕顶部xy坐标
+  const getNodeTop = () => {
+    // console.log(imgRef)
+    const viewTop = getScrollTop();
+    let nodeTop: number = 0;
+    let nodeBottom: number = 0
+    if (imgRef.current) {
+      nodeTop = imgRef.current.getBoundingClientRect().top + viewTop
+      nodeBottom = imgRef.current.offsetHeight + nodeTop;
+    }
+    // console.log(nodeTop, nodeBottom)
+    return ({
+      nodeTop, nodeBottom
+    })
+  }
+
+
+  // 滚动事件
+  function _handleScroll() {
+    const { offset = 0 } = props;
+    // 当前图片的顶部和底部到网页最顶部的距离
+    const { nodeTop, nodeBottom } = getNodeTop();
+    // 当前网页到顶部的距离
+    const viewTop = getScrollTop();
+    // 当前网页底部到最顶部的距离
+    const viewBottom = viewTop + getClientHeight();
+    if (nodeBottom + offset >= viewTop && nodeTop - offset <= viewBottom) {
+      setIsLoaded(true)
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleScroll);
+    }
+
+  }
+
+
+  const testThrottle = useCallback(() => {
+    const fn = throttle(() => console.log(222), 1000)
+    return fn
+  }, [])
+
+  // const refCallback = useCallback((node) => {
+  //   if (node) {
+  //     setImgRef(node)
+  //   }
+  // }, [])
+
+  const { placeholder, offset, ...imgProps } = props;
+  return (
+    <>
+      {/* <Button type="dashed" onClick={testThrottle()}>测试节流</Button> */}
+      <div ref={imgRef}>
+        {isLoaded ? <img alt="这是一个图片"  {...imgProps} /> : <span>{placeholder}</span>}
+      </div>
+    </>
+  )
+}
+export default LazyImage
